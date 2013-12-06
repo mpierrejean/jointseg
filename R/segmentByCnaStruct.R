924d0f1d8d60b986060c0e73755e75f0c279773a
Pierre Neuvial <pierre.neuvial@genopole.cnrs.fr>
2013-12-06 23:33:16 +0000

    Minor changes (doc).

--- R/segmentByCnaStruct.R
+++ R/segmentByCnaStruct.R
@@ -31,6 +31,11 @@ segmentByCnaStruct <- structure(function(## Run cnaStruct segmentation
   if (!require("CnaStruct")) {
     stop("Please install the 'CnaStruct' package to run the 'segmentByCnaStruct' function\nURL:http://genomics.cicbiogune.es/cnastruct")
   }
+
+  ## sanity check
+  if (maxk*K<=nrow(Y)) {
+    warning("'CnaStruct' requires that maxk*K>nrow(Y) in order to give meaningful results")
+  }
   cn <- colnames(Y)
   ecn <- c("c", "b") ## expected
   mm <- match(ecn, cn)
@@ -66,7 +71,7 @@ segmentByCnaStruct <- structure(function(## Run cnaStruct segmentation
     ## run CnaStruct segmentation
     Y <- cbind(c=log2(datS[, "c"])-1, b=datS[, "b"])  ## Convert to log ('LRR') scale
     res <- jointSeg:::segmentByCnaStruct(Y, K=K*10, maxk=500)  
-    getTpFp(res$bkp, sim$bkp, tol=5, relax = -1)   ## true and false positives
+    getTpFp(res$bkp, sim$bkp, tol=5, relax=-1)   ## true and false positives
     plotSeg(datS, breakpoints=list(sim$bkp, res$bkp))
   }
 })
