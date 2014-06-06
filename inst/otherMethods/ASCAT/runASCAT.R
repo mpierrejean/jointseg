3e8133b64206b4acfca5696621fa60edfd4362fd
Pierre Neuvial <pierre.neuvial@genopole.cnrs.fr>
2014-06-06 14:23:53 +0000

    Updated to doc.

--- inst/otherMethods/ASCAT/runASCAT.R
+++ inst/otherMethods/ASCAT/runASCAT.R
@@ -16,17 +16,20 @@ K <- 20 ## Number of breakpoints
 sim <- getCopyNumberDataByResampling(n, K, regData=dat)
 datS <- sim$profile
 
+## ASCAT segmentation
 system.time(res <- PSSeg(datS, method="ASCAT"))
 res$bestBkp
-
 plotSeg(datS, list(sim$bkp, res$bestBkp))
 
+## Compare with RBS+DP
 system.time(res2 <- PSSeg(datS, method="RBS", stat=c("c", "d"), K=100))
 res2$bestBkp
 
+## performance assessment as a function of tolerance
 sapply(0:5, FUN=function(tol) getTpFp(res2$bestBkp, sim$bkp, tol=tol))
 sapply(0:5, FUN=function(tol) getTpFp(res$bestBkp, sim$bkp, tol=tol))
 
+## plot a ROC curve for a given tolerance value
 tol <- 0
 bkpList <- res$dpBkpList
 bkpList2 <- res2$dpBkpList
@@ -34,6 +37,7 @@ bkpList2 <- res2$dpBkpList
 roc <- sapply(bkpList, getTpFp, sim$bkp, tol)
 roc2 <- sapply(bkpList2, getTpFp, sim$bkp, tol)
 
-plot(roc["FP", ], roc["TP", ], t='s', ylim=c(0, 12))
+ylim <- c(0, max(roc["TP", ], roc2["TP", ]))
 
+plot(roc["FP", ], roc["TP", ], t='s', ylim=ylim)
 lines(roc2["FP", ], roc2["TP", ], t='s', col=2)
